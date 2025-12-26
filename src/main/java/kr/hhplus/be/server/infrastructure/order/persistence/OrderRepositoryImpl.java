3938ba1e687aea56dc573c45cbe7365435543916
Jin0207 <86141192+Jin0207@users.noreply.github.com>
2025-12-26 14:55:16 +0900

    Feat: 주문 관련 repository 구현

--- src/main/java/kr/hhplus/be/server/infrastructure/order/persistence/OrderRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/order/persistence/OrderRepositoryImpl.java
@@ -1,10 +1,12 @@
 package kr.hhplus.be.server.infrastructure.order.persistence;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import kr.hhplus.be.server.domain.order.entity.Order;
+import kr.hhplus.be.server.domain.order.enums.OrderStatus;
 import kr.hhplus.be.server.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -15,16 +17,28 @@ public class OrderRepositoryImpl implements OrderRepository {
     private final OrderJpaRepository jpaRepository;
 
     @Override
-    public Optional<Order> findById(Long id) {
-        // TODO Auto-generated method stub
-        return Optional.empty();
+    public Order save(Order order) {
+        OrderEntity entity = OrderEntity.from(order);
+        return jpaRepository.save(entity).toDomain();
     }
 
     @Override
-    public Order save(Order order) {
-        // TODO Auto-generated method stub
-        return null;
+    public Optional<Order> findById(Long id) {
+        return jpaRepository.findById(id)
+            .map(OrderEntity::toDomain);
     }
 
+    @Override
+    public List<Order> findByUserId(Long userId) {
+        return jpaRepository.findByUserId(userId).stream()
+            .map(OrderEntity::toDomain)
+            .toList();
+    }
 
+    @Override
+    public List<Order> findByOrderStatus(OrderStatus status) {
+        return jpaRepository.findByOrderStatus(status).stream()
+            .map(OrderEntity::toDomain)
+            .toList();
+    }
 }
