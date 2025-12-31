721bc539e1e012d27dd5a9896a83106f3e707fc2
Jin0207 <86141192+Jin0207@users.noreply.github.com>
2025-12-31 14:42:14 +0900

    Chore: 설정 파일 및 인프라 구성

--- src/main/java/kr/hhplus/be/server/infrastructure/config/RedisConfig.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/config/RedisConfig.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.infrastructure.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,12 +11,13 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Profile("!test")  // 테스트 환경에서는 이 설정을 사용하지 않음
 public class RedisConfig {
 
-	@Value("${spring.redis.host}")
+	@Value("${spring.data.redis.host}")
 	private String host;
 
-	@Value("${spring.redis.port}")
+	@Value("${spring.data.redis.port}")
 	private int port;
 
 	@Bean
